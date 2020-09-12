6683282437825bc9116928a15b15150dba873458
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-12 16:07:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,7 +18,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-                 return shipping_manifest["oil paintings"]
+    return shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -27,14 +27,8 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+    shipping_manifest["muskets"] = 2
+    shipping_manifest["gun powder"] = 4
 
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+    return shipping_manifest
 end
